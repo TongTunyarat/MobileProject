e38deda2d943e3570c197441e6b8a042eba99953
Tunyarat Nitihirunpattana <113578736+TongTunyarat@users.noreply.github.com>
2023-04-25 10:16:52 +0700

    Update main.dart

--- project_v1/lib/main.dart
+++ project_v1/lib/main.dart
@@ -1,24 +1,12 @@
 import 'package:flutter/material.dart';
-
-
 import 'package:firebase_core/firebase_core.dart';
-//import 'package:project_v1/Login/meal_list.dart';
-import 'package:project_v1/APIpage/api_fooddetail.dart';
 import 'package:project_v1/APIpage/api_listview.dart';
-import 'package:project_v1/controller/getdata_provider.dart';
 import 'package:project_v1/Login/signin_page.dart';
 import 'package:project_v1/Login/singup_page.dart';
-import 'package:provider/provider.dart';
-
 import 'intro.dart';
-//import 'search_food.dart';
 import 'home.dart';
 import 'Game/opencard.dart';
 import 'Game/spin.dart';
-
-//import 'package:firebase_options.dart';
-
-
 
 void main()  async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -36,11 +24,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-        providers: [
-          ChangeNotifierProvider(create: (ctx) => GetDataProvider()),
-        ],
-      child: MaterialApp(
+    return MaterialApp(
         title: 'Flutter Demo',
         debugShowCheckedModeBanner: false,
         theme: ThemeData(
@@ -51,7 +35,6 @@ class MyApp extends StatelessWidget {
           '/': (BuildContext context) => const IntroductionScreen(),
           '/home': (BuildContext context) => const HomePage(),
           '/foodlist': (BuildContext context) =>  MealsList(),
-          '/fooddetail': (BuildContext context) => My_search(),
           '/searchfood': (BuildContext context) => const SpinPage(),
           '/spin': (BuildContext context) => const SpinPage(),
           '/opencard': (BuildContext context) => const OpenTheCard(),
@@ -59,7 +42,6 @@ class MyApp extends StatelessWidget {
           '/signup': (BuildContext context) =>  singup_Page(),
         },
         initialRoute: "/", //ให้เปิดappขึ้นมาหน้าแรก
-      ),
-    );
+      );
   }
 }

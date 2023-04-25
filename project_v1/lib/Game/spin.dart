338367fc4e8f6c4fb0e7449f2db4961ccd0474db
Tunyarat Nitihirunpattana <113578736+TongTunyarat@users.noreply.github.com>
2023-04-25 09:51:09 +0700

    Update spin.dart

--- project_v1/lib/Game/spin.dart
+++ project_v1/lib/Game/spin.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_fortune_wheel/flutter_fortune_wheel.dart';
-
 class SpinPage extends StatefulWidget {
   const SpinPage({super.key});
   @override
@@ -47,108 +46,118 @@ class _SpinPageState extends State<SpinPage> with TickerProviderStateMixin {
           title: const Text(
             'SPIN',
             style: TextStyle(
-                fontSize: 35, color: Colors.black, fontWeight: FontWeight.bold),
+                fontSize: 35, color: Color.fromARGB(255, 20, 20, 20), fontWeight: FontWeight.bold),
           ),
-          backgroundColor: Color.fromARGB(255, 255, 255, 255),
+          backgroundColor: Color.fromARGB(255, 251, 250, 250),
           leading: const CircleAvatar(
-            backgroundColor: Color.fromARGB(255, 255, 255, 255),
+            backgroundColor: Colors.transparent,
             radius: 20,
             child: BackButton(
-              color: Colors.black,
+              color: Color.fromARGB(255, 20, 20, 20),
             ),
           ),
         ),
-        body: SafeArea(
-          child: Stack(children: [
-            Padding(
-              padding: const EdgeInsets.only(top: 30, left: 50, bottom: 450),
-              child: Image.asset("lib/image/gametime.png", width: 300),
-            ),
-            Align(
-                alignment: Alignment.bottomCenter,
-                child: Container(
-                  height: MediaQuery.of(context).size.height * 0.65,
-                  width: MediaQuery.of(context).size.width,
-                  decoration: const BoxDecoration(
-                      color: Colors.black87,
-                      borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(50),
-                          topRight: Radius.circular(50))),
-                  child: Column(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Container(
-                          child: Text(
-                            "Let's play!! to choose a food",
-                            style: TextStyle(
-                              fontSize: 20,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white,
+        body: Container(
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage("assets/images/1.png"),
+                  colorFilter: ColorFilter.mode(
+                    Colors.white.withOpacity(0.75),
+                    BlendMode.modulate,
+                  ),
+                  fit: BoxFit.cover)),
+          child: SafeArea(
+            child: Stack(children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 30, left: 50, bottom: 450),
+                child: Image.asset("lib/image/gametime.png", width: 300),
+              ),
+              Align(
+                  alignment: Alignment.bottomCenter,
+                  child: Container(
+                    height: MediaQuery.of(context).size.height * 0.65,
+                    width: MediaQuery.of(context).size.width,
+                    decoration: const BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(50),
+                            topRight: Radius.circular(50))),
+                    child: Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Container(
+                            child: Text(
+                              "Let's play!! to choose a food",
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black,
+                              ),
+                            ),
+                            alignment: Alignment.topCenter,
+                          ),
+                        ),
+                        Expanded(
+                          child: Padding(
+                            padding: const EdgeInsets.only(top: 30),
+                            child: FortuneWheel(
+                              selected: selected.stream,
+                              items: [
+                                for (var it in items)
+                                  FortuneItem(
+                                      child: Text(
+                                    it,
+                                    style: TextStyle(color: Colors.black),
+                                  )),
+                              ],
+                              // styleStrategy: const UniformStyleStrategy(
+                              //   borderColor: Colors.white,
+                              //   color: Colors.,
+                              // ),
+                              indicators: const [
+                                FortuneIndicator(
+                                  alignment: Alignment.topCenter,
+                                  child: TriangleIndicator(
+                                    color: Colors.black54,
+                                  ),
+                                ),
+                              ],
                             ),
                           ),
-                          alignment: Alignment.topCenter,
                         ),
-                      ),
-                      Expanded(
-                        child: Padding(
-                          padding: const EdgeInsets.only(top: 30),
-                          child: FortuneWheel(
-                            selected: selected.stream,
-                            items: [
-                              for (var it in items)
-                                FortuneItem(
-                                    child: Text(
-                                  it,
-                                  style: TextStyle(color: Colors.white),
-                                )),
-                            ],
-                            // styleStrategy: const UniformStyleStrategy(
-                            //   borderColor: Colors.white,
-                            //   color: Colors.,
-                            // ),
-                            indicators: const [
-                              FortuneIndicator(
-                                alignment: Alignment.topCenter,
-                                child: TriangleIndicator(
-                                  color: Colors.white,
-                                ),
-                              ),
-                            ],
+                        GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              selected.add(
+                                Fortune.randomInt(0, items.length),
+                              );
+                            });
+                          },
+                          child: Padding(
+                            padding: EdgeInsets.only(bottom: 50, top: 50),
+                            child: Container(
+                              height: 50,
+                              width: 150,
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(60),
+                                  color: Colors.amber[800]),
+                              child: Center(
+                                  child: Text(
+                                "START",
+                                style: TextStyle(
+                                    fontSize: 20,
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold),
+                              )),
+                            ),
                           ),
                         ),
-                      ),
-                      GestureDetector(
-                        onTap: () {
-                          setState(() {
-                            selected.add(
-                              Fortune.randomInt(0, items.length),
-                            );
-                          });
-                        },
-                        child: Padding(
-                          padding: EdgeInsets.only(bottom: 50, top: 50),
-                          child: Container(
-                            height: 50,
-                            width: 150,
-                            decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(60),
-                                color: Colors.redAccent),
-                            child: Center(
-                                child: Text(
-                              "START",
-                              style: TextStyle(
-                                  fontSize: 20,
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.bold),
-                            )),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                )),
-          ]),
+                      ],
+                    ),
+                  )),
+            ]),
+          ),
         ));
   }
 }
